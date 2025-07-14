2356e0e5e221993a4145ffa5eebd15b34050d8ff
clare <me@clare.fyi>
2025-07-14 12:06:10 -0400

    .

--- main.go
+++ main.go
@@ -19,28 +19,25 @@ type (
 	M struct {
 		Title string    `yaml:"title"`
 		Date  time.Time `yaml:"date"`
+		Filename string
 	}
 )
 
 func main() {
 	renderBlog()
-	renderIndex()
+	renderPage("index.html")
 }
 
-func renderIndex() {
-	t, e := template.ParseFiles("s/t/base.html")
-	if e != nil {
-		log.Fatal("couldn't open base template")
-	}
-	f, e := os.ReadFile("s/t/index.html")
-	if e != nil {
-		log.Fatal("couldn't open index.html")
-	}
+func renderPage(f string) {
+	t,e:=template.ParseFiles("s/t/base.html")
+	if e!=nil { log.Fatal("couldn't open base template") }
+	b, e := os.ReadFile("s/t/"+f)
+	if e != nil { log.Fatal("couldn't open "+f) }
 	var buf bytes.Buffer
-	if e := t.Execute(&buf, D{T: template.HTML(f)}); e != nil {
-		log.Fatal("couldn't execute index template")
+	if e := t.Execute(&buf, D{T: template.HTML(b)}); e!=nil {
+		log.Fatal("couldn't execute base tmpl + "+f)
 	}
-	os.WriteFile("s/index.html", buf.Bytes(), 0o644)
+	os.WriteFile("s/"+f,buf.Bytes(),0o644)
 }
 
 func renderBlog() {
@@ -52,6 +49,7 @@ func renderBlog() {
 	if e != nil {
 		log.Fatal("couldn't open post template")
 	}
+	var posts []M
 	filepath.WalkDir("posts", func(p string, d fs.DirEntry, e error) error {
 		if e != nil || d.IsDir() || !strings.HasSuffix(d.Name(), ".md") {
 			return e
@@ -60,19 +58,24 @@ func renderBlog() {
 		if e != nil {
 			return e
 		}
+
 		var meta M
 		rest, e := frontmatter.Parse(f, &meta)
 		if e!=nil{return e}
+		meta.Filename = strings.TrimSuffix(d.Name(), ".md")
+		posts = append(posts, meta)
+
 		var buf, out bytes.Buffer
 		if e := post.Execute(&buf, meta); e!=nil {return e}
 		if e := goldmark.Convert(rest, &buf); e != nil {
 			return e
 		}
 		b := bytes.NewBuffer(bytes.TrimSpace(buf.Bytes()))
+
 		if e := t.Execute(&out, D{T: template.HTML(b.Bytes())}); e != nil {
 			return e
 		}
-		o := filepath.Join("s", strings.TrimSuffix(d.Name(), ".md")+".html")
+		o := filepath.Join("s", meta.Filename+".html")
 		os.WriteFile(o, out.Bytes(), 0o644)
 
 		return nil
